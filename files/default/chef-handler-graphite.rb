bfce2e41d9c3ae8ee9b75b3f7273c142de33dc08
Peter Donald <peter@realityforge.org>
2015-08-21 07:53:29 +1000

    Guard against missing method in some versions of chef.
    Fixes #9

--- files/default/chef-handler-graphite.rb
+++ files/default/chef-handler-graphite.rb
@@ -57,7 +57,7 @@ class GraphiteReporting < Chef::Handler
     end
 
     # user provided metrics
-    user_metrics = run_status.run_context[:graphite_handler_metrics]
+    user_metrics = run_status.run_context.respond_to?(:[]) ? run_status.run_context[:graphite_handler_metrics] : {}
     metrics.merge!(user_metrics) if user_metrics.is_a?(Hash)
 
     if @enable_profiling
